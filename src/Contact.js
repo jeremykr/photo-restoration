292bc98f07ce6d876726edfca1231528ff63f62d
jeremykr <jeremy.krenbrink@gmail.com>
2018-05-08 17:57:17 -0600

    Revert "Add forms to Contact tab"
    This reverts commit dbd9e1e36648e621bfb7aaca079a62f29c51040f.

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import ContactForm from './ContactForm';
-import './Contact.css';
 
 export default class Contact extends Component {
     render() {
         return (
-            <span>
-                <h1 className='Contact-header'>Contact me!</h1>
-                <ContactForm/>
-            </span>
+            <p>Contact</p>
         );
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <h1 className='Home-header'>Welcome</h1>
+            <p>Home</p>
         );
     }
 }
