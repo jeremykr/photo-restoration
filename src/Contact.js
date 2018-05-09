9c8cff7e6dc3dd391e5ce883e5e7b382b2683ca9
jeremykr <jeremy.krenbrink@gmail.com>
2018-05-09 11:37:54 -0600

    Add space for contact details and bio

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './Contact.css';
 
 export default class Contact extends Component {
     render() {
         return (
-            <p>Contact</p>
+            <span className='Contact'>
+                <h1 className='Contact-header'>Contact me!</h1>
+                <div className='Contact-info-box'>
+                    <div id="Contact-email" className="Contact-info">
+                        <strong>Email:</strong> not_a_real_email@website.com
+                    </div>
+                    <div id="Contact-phone" className="Contact-info">
+                        <strong>Phone:</strong> (123) 456-7890 
+                    </div>
+                </div>
+            </span>
         );
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <p>Home</p>
+            <span className="Home">
+                <h1 id='Home-logo'>LOGO</h1>
+                <div id='Home-bio'>
+                    <p>
+                    Vel dicant minimum in, et saepe detracto usu. Duis corpora appellantur te mei, aeque iusto eam te,<br/>
+                    mea ea dicat dolores erroribus. Illud impedit concludaturque eam ad. At probo legendos sea, ius ad solum<br/>
+                    utamur iisque, sea et choro pertinacia neglegentur.<br/>
+                    <br/>
+                    Invidunt erroribus philosophia quo an, vix quando assueverit te, eu omnium integre sit. Te illud dolorum<br/>
+                    vix. Usu oportere principes an, ut tota dicunt splendide mel, sit et vidisse appetere. Purto delectus<br/>
+                    signiferumque ne vim, in option tincidunt moderatius per. Per inani feugait scaevola an, ius iusto regione et.
+                    </p>
+                </div>
+            </span>
         );
     }
 }
